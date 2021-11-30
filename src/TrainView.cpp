c75c2d4646a9d70e8c95c51c3bdac1a3ab94fbfc
Rhodanthe1116 <z0916945857@gmail.com>
2021-12-01 04:17:54 +0800

    cubic b spline

--- src/TrainView.cpp
+++ src/TrainView.cpp
@@ -26,6 +26,9 @@
 
 #include <iostream>
 #include <Fl/fl.h>
+#include <vector>
+#include <algorithm>
+#include <tuple>
 
 // we will need OpenGL, and OpenGL needs windows.h
 #include <windows.h>
@@ -333,22 +336,297 @@ setProjection()
 	}
 }
 
+
+
+vector<double> normalized(vector<double> arr) {
+	double max = *std::max_element(arr.begin(), arr.end());
+	double min = *std::min_element(arr.begin(), arr.end());
+	std::transform(arr.begin(), arr.end(), arr.begin(), [max, min](double num) {
+		return  (num - min) / (max - min);
+		});
+	return arr;
+}
+// t: 0~1
+// w: 0~w
+vector<vector<double>> calCubicBSplineN(
+	int n,
+	int k,
+	vector<double> pt,
+	// Total number of points
+	int w
+	// scaleOfValue = 100,
+) {
+	// A knot vector (t0 , t1 , ... , tn+k )
+	const int sizeOfT = n + k + 1;
+	vector<vector<double>>  N(w, std::vector<double>(sizeOfT, 0.0));
+
+	// t: 0~1
+	vector<double> t = normalized(pt);
+
+	const double dtPerW = (t[sizeOfT - 1] - t[0]) / w;
+	int i1 = 0;
+	double _t = t[0];
+	for (int _w = 0; _w < w; _w++) {
+		while (_t >= t[i1]) i1++;
+		const int i = i1 - 1;
+		N[_w][i] = 1;
+		// k bottom-up
+		for (int _k = 2; _k <= k; _k++) {
+			//  basis functions calculation
+			int jb = i - _k + 1;
+			if (jb < 0) jb = 0;
+			for (int ii = jb; ii <= i; ii++)
+			{
+
+				if (ii + _k >= sizeOfT) {
+					continue;
+				}
+				// ºu°Ê
+				N[_w][ii] =
+					(N[_w][ii] * (_t - t[ii])) / (t[ii + _k - 1] - t[ii]) +
+					(N[_w][ii + 1] * (t[ii + _k] - _t)) / (t[ii + _k] - t[ii + 1]);
+			}
+		}
+		_t += dtPerW;
+	}
+
+	return N;
+};
+
+std::tuple <vector<double>, vector<double>> calCubicBSplineP(
+	int n,
+	int k,
+	vector<double> ptX,
+	vector<double> ptY,
+	vector<double> pt,
+	// Total number of points
+	int w,
+	// scaleOfValue = 100,
+	vector<vector<double>>  N
+) {
+	// A knot vector (t0 , t1 , ... , tn+k )
+	vector<double> Px = ptX;
+	vector<double> Py = ptY;
+	vector<double> ti = normalized(pt);
+
+	const int startW = floor((ti[k - 1] - ti[0]) / w) + 1;
+	int currentSplineStartW = startW;
+
+	vector<double> pointXsInW(w);
+	vector<double> pointYsInW(w);
+
+	double sX = 0;
+	double sY = 0;
+	for (int i = 0; i < n + 1; i++) {
+		sX += Px[i] * N[currentSplineStartW][i];
+		sY += Py[i] * N[currentSplineStartW][i];
+	}
+	pointXsInW[currentSplineStartW] = sX;
+	pointYsInW[currentSplineStartW] = sY;
+
+	// for spline
+	for (int curSpline = k - 1; curSpline < n + 1; curSpline++) {
+		const double currentSplineEndT = ti[curSpline + 1] - ti[0];
+		const int currentEndW = floor(currentSplineEndT * w);
+
+		// draw points in spline
+		for (int _w = currentSplineStartW; _w < currentEndW; _w++) {
+			sX = sY = 0;
+			for (int i = 0; i < n + 1; i++) {
+				sX += Px[i] * N[_w][i];
+				sY += Py[i] * N[_w][i];
+			}
+			pointXsInW[_w] = sX;
+			pointYsInW[_w] = sY;
+		}
+		currentSplineStartW = currentEndW;
+	}
+
+	return make_tuple(pointXsInW, pointYsInW);
+}
+
+
+void drawSpline(
+	int n,
+	int k,
+	vector<double> ptX,
+	vector<double> ptY,
+	vector<double> pt,
+	// Total number of points
+	int w,
+	int h,
+	int totalNumOfPoints,
+	// scaleOfValue = 100,
+	vector<double>  pointXsInW,
+	vector<double> pointYsInW
+) {
+	// A knot vector (t0 , t1 , ... , tn+k )
+	vector<double> Px = ptX;
+	vector<double> Py = ptY;
+	vector<double> ti = normalized(pt);
+	const int h1 = h - 1;
+
+
+	// Draw linear control point lines
+	//glBegin(GL_LINE_STRIP);
+	//for (int i = 1; i < n + 1; i++) {
+
+	//	glVertex3f(Px[i] * w, 1, Py[i] * h);
+
+	//}
+	//glEnd();
+
+
+
+
+	// Draw splines
+	glLineWidth(5);
+	const int startW = floor((ti[k - 1] - ti[0]) * totalNumOfPoints) + 1;
+	int currentSplineStartW = startW;
+	// for spline
+	for (int curSpline = k - 1; curSpline < n + 1; curSpline++) {
+		glBegin(GL_LINE_STRIP);
+		const double currentEndT = (ti[curSpline + 1] - ti[0]);
+
+		const int currentSplineEndW = floor(currentEndT * totalNumOfPoints);
+		std::cout << "currentSplineStartW " << currentSplineStartW << " " << currentSplineEndW << std::endl;
+		glColor3f(double(curSpline % 10) / 10, 1 - double(curSpline % 10) / 10, double(curSpline % 4) / 5);
+		/*	ctx.strokeStyle = iColor[(curSpline - k + 1) % 7];
+			ctx.beginPath();
+			ctx.moveTo(
+				pointXsInW[currentSplineStartW] * w,
+				h - pointYsInW[currentSplineStartW] * h,
+				);*/
+				// draw points in spline
+		for (int _w = currentSplineStartW; _w < currentSplineEndW; _w++) {
+			glVertex3f(pointXsInW[_w] * w, 1, pointYsInW[_w] * h);
+		}
+
+		//ctx.stroke();
+		currentSplineStartW = currentSplineEndW;
+		glEnd();
+	}
+
+	// draw control points
+	/*ctx.lineWidth = d;
+	ctx.strokeStyle = '#0000f0';
+	for (let i = 0; i < n + 1; i++)
+		ctx.strokeRect(Px[i] * w - d, h - Py[i] * h - d, d2, d2);*/
+
+	for (int i = 0; i < Px.size(); i++) {
+		drawCube(Px[i], 5, Py[i], 5);
+
+	}
+}
+
 void TrainView::drawTrack(bool doingShadows)
 {
-	// Draw the control points
-	// don't draw the control points if you're driving 
-	// (otherwise you get sea-sick as you drive through them)
-	if (!tw->trainCam->value()) {
-		for (size_t i = 0; i < m_pTrack->points.size(); ++i) {
-			if (!doingShadows) {
-				if (((int)i) != selectedCube)
-					glColor3ub(240, 60, 60);
-				else
-					glColor3ub(240, 240, 30);
-			}
-			m_pTrack->points[i].draw();
+
+	vector<ControlPoint> controlPoints = m_pTrack->points;
+
+	if (false) {
+
+		// Create and draw the curves for every 4 points
+		// Use GL_LINE_STRIP instead to fill the gaps
+		glBegin(GL_LINE_LOOP);
+		// Draw the curve
+		for (int i = 0; i < controlPoints.size(); i++) {
+			ControlPoint point = controlPoints[i];
+			glVertex3f(point.pos.x, point.pos.y, point.pos.z);
 		}
+		glEnd();
 	}
+
+
+	if (controlPoints.size() < 4) {
+		return;
+	}
+	int n = controlPoints.size() + 2;
+	int k = 4;
+	/*vector<double> ptX = { 0.2, 0.8, 0.8, 0.2, 0.8, 0.8 };
+	vector<double> ptY = { 0.9, 0.9, 0.2, 0.9, 0.9, 0.2 };
+	vector<double> pt = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };*/
+	vector<double> ptX;
+	vector<double> ptY;
+	for (int i = 0; i < controlPoints.size(); i++)
+	{
+		ControlPoint point = controlPoints[i];
+		ptX.push_back(point.pos.x);
+		ptY.push_back(point.pos.z);
+	}
+	// To make a C k-2 continuous closed loop you need only, that the last k - 1 control points repeat the first k - 1 ones, i.e. [P0 , P1 , P2 , P0 , P1 , P2 ] for n = 5, k = 4 (in this example the last 3 points are displaced a bit to make them visible).
+	for (int i = 0; i < k - 1; i++)
+	{
+		ControlPoint point = controlPoints[i];
+		ptX.push_back(point.pos.x);
+		ptY.push_back(point.pos.z);
+
+	}
+
+	vector<double> pt;
+	int sizeOfT = n + k + 1;
+	for (int i = 0; i < sizeOfT; i++)
+	{
+		pt.push_back(i);
+
+	}
+	int w = 1;
+	int h = 1;
+	int totalNumOfPoints = 400;
+	auto N = calCubicBSplineN(n, k, pt, totalNumOfPoints);
+	/*for (int _w = 0; _w < 10; _w++)
+	{
+		for (int i = 0; i < n + k + 1; i++)
+		{
+			std::cout << N[_w][i] << " ";
+
+		}
+		std::cout << std::endl;
+	}
+	std::cout << std::endl;*/
+	vector<double> pointXsInW, pointYsInW;
+	tie(pointXsInW, pointYsInW) = calCubicBSplineP(n, k, ptX, ptY, pt, totalNumOfPoints, N);
+	/*for (int i = 0; i < pointXsInW.size(); i++)
+	{
+		std::cout << pointXsInW[i] << " ";
+	}
+	std::cout << std::endl;*/
+
+	// drawFun(N);
+	drawSpline(n, k, ptX, ptY, pt, w, h, totalNumOfPoints, pointXsInW, pointYsInW);
+	for (int i = 0; i < ptX.size(); i++)
+	{
+		std::cout << ptX[i] << " " << ptY[i] << std::endl;
+	}
+	std::cout << std::endl;
+
+	vector<double> ti = normalized(pt);
+
+	double trainT = m_pTrack->trainU;
+	double numOfTrackPoints = double((n + 1) - (k - 1)) * totalNumOfPoints / (sizeOfT - 1);
+	const int startW = floor((ti[k - 1] - ti[0]) * totalNumOfPoints);
+	int trainW = startW + (trainT * numOfTrackPoints);
+	//std::cout << "numOfTrackPoints" << numOfTrackPoints << std::endl;
+	//std::cout << "trainW" << trainW << std::endl;
+	double trainX = pointXsInW[trainW % totalNumOfPoints];
+	double trainY = pointYsInW[trainW % totalNumOfPoints];
+	drawCube(trainX, +5, trainY, 5);
+	std::cout << m_pTrack->trainU << std::endl;
+
+}
+
+void TrainView::drawTrain(bool doingShadows)
+{
+	vector<ControlPoint> controlPoints = m_pTrack->points;
+	drawCube(controlPoints[0].pos.x, controlPoints[0].pos.y + 5, controlPoints[0].pos.z, 5);
+	m_pTrack->trainU;
+	//glBegin(GL_LINE_LOOP);
+	//// Draw the curve
+	//for (int i = 0; i < controlPoints.size(); i++) {
+	//	ControlPoint point = controlPoints[i];
+	//	glVertex3f(point.pos.x, point.pos.y, point.pos.z);
+	//}
+	//glEnd();
 
 }
 
@@ -393,11 +671,11 @@ void TrainView::drawStuff(bool doingShadows)
 	// TODO: 
 	//	call your own train drawing code
 	//####################################################################
-#ifdef EXAMPLE_SOLUTION
 	// don't draw the train if you're looking out the front window
-	if (!tw->trainCam->value())
-		drawTrain(this, doingShadows);
-#endif
+	if (!tw->trainCam->value()) {
+		this->drawTrain(doingShadows);
+
+	}
 }
 
 
